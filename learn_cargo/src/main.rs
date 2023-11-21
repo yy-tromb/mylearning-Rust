487fb131aabbff1f0eec7b29413a006cca636571
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-11-22 01:00:09 +0900

    hoge

--- learn_cargo/src/main.rs
+++ learn_cargo/src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+#[derive(Debug)]
+enum OS {
+    Windows,
+    Mac,
+    Linux(String),
+}
+
 fn main() {
     println!("Hello, world!");
+    let x = 50;
+    if x == 50 {
+        println!("{}", x);
+        x.to_string();
+    } else {
+        println!("no");
+        "no".to_string();
+    }
+    println!("{}", x);
+    let point = Point { x: 32, y: 32 };
+    println!("{:#?}", point);
+    let os = OS::Linux("Ubuntu".to_string());
+    println!("{:#?}",os)
 }
